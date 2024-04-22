ac99e89c88ec12dcc1e209d4d6798d29526e740c
Mar1aP <92302738+Nintorch@users.noreply.github.com>
2024-04-22 11:42:56 +0500

    Main menu (employees list) prototype

--- App/MainMenuForm.Designer.cs
+++ App/MainMenuForm.Designer.cs
@@ -29,6 +29,24 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            dataGridView1 = new DataGridView();
+            textBox1 = new TextBox();
+            groupBox1 = new GroupBox();
+            label2 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            checkBox1 = new CheckBox();
+            dateTimePicker1 = new DateTimePicker();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
@@ -41,14 +59,170 @@
             label1.TabIndex = 2;
             label1.Text = "Список сотрудников ресторана";
             // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 40);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(532, 367);
+            dataGridView1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(6, 46);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(225, 27);
+            textBox1.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(dateTimePicker1);
+            groupBox1.Controls.Add(checkBox1);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Location = new Point(550, 40);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(238, 367);
+            groupBox1.TabIndex = 5;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Поиск сотрудников";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 23);
+            label2.Name = "label2";
+            label2.Size = new Size(76, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Фамилия:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 76);
+            label3.Name = "label3";
+            label3.Size = new Size(42, 20);
+            label3.TabIndex = 7;
+            label3.Text = "Имя:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(6, 99);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(225, 27);
+            textBox2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(6, 129);
+            label4.Name = "label4";
+            label4.Size = new Size(75, 20);
+            label4.TabIndex = 9;
+            label4.Text = "Отчество:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(6, 152);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(225, 27);
+            textBox3.TabIndex = 8;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(6, 182);
+            label5.Name = "label5";
+            label5.Size = new Size(89, 20);
+            label5.TabIndex = 11;
+            label5.Text = "Должность:";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(6, 205);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(225, 27);
+            textBox4.TabIndex = 10;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(6, 238);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(141, 24);
+            checkBox1.TabIndex = 13;
+            checkBox1.Text = "Дата рождения:";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(6, 268);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(225, 27);
+            dateTimePicker1.TabIndex = 14;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(6, 301);
+            button1.Name = "button1";
+            button1.Size = new Size(225, 29);
+            button1.TabIndex = 15;
+            button1.Text = "Найти";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 413);
+            button2.Name = "button2";
+            button2.Size = new Size(225, 29);
+            button2.TabIndex = 16;
+            button2.Text = "Добавить нового сотрудника";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(243, 413);
+            button3.Name = "button3";
+            button3.Size = new Size(200, 29);
+            button3.TabIndex = 17;
+            button3.Text = "Удалить сотрудника";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(449, 413);
+            button4.Name = "button4";
+            button4.Size = new Size(200, 29);
+            button4.TabIndex = 18;
+            button4.Text = "Сохранить изменения";
+            button4.UseVisualStyleBackColor = true;
+            // 
             // MainMenuForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(groupBox1);
+            Controls.Add(dataGridView1);
             Controls.Add(label1);
             Name = "MainMenuForm";
-            Text = "MainMenuForm";
+            Tag = "";
+            Text = "Сотрудники ресторана - Список сотрудников";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -56,5 +230,21 @@
         #endregion
 
         private Label label1;
+        private DataGridView dataGridView1;
+        private TextBox textBox1;
+        private GroupBox groupBox1;
+        private Button button1;
+        private DateTimePicker dateTimePicker1;
+        private CheckBox checkBox1;
+        private Label label5;
+        private TextBox textBox4;
+        private Label label4;
+        private TextBox textBox3;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label2;
+        private Button button2;
+        private Button button3;
+        private Button button4;
     }
 }
